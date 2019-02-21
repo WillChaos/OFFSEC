9a7bbab6c7f2f272b22edc36d533d512fe8687b7
WillChaos <46702070+WillChaos@users.noreply.github.com>
2019-02-21 19:13:20 +0800

    Update SMB-Enum.sh
    Syntax fix 2 - lolololol

--- Enumeration/SMB-Enum.sh
+++ Enumeration/SMB-Enum.sh
@@ -10,19 +10,19 @@ if [ -z "$1" ]
 
     # checks for shares via SMB
     echo "[*] Looking up share names..."
-    nmap –script smb-enum-shares.nse –script-args=unsafe=1 -p445 $1
+    nmap --script smb-enum-shares.nse --script-args=unsafe=1 -p445 $1
     
     # checks for users via SMB
     echo "[*] Looking up potential user names..."
-    nmap –script smb-enum-users.nse –script-args=unsafe=1 -p445 $1
+    nmap --script smb-enum-users.nse --script-args=unsafe=1 -p445 $1
     
     # checks for MS17-010
     echo "[*] Checking exlusively for MS17-010"
-    nmap -p 445 -script=smb-vuln-ms17-010.nse $1
+    nmap -p 445 --script=smb-vuln-ms17-010.nse $1
 
     # checks for well know SMB vulnerabilities
     echo "[*] Checking for known easy to probe vulnerabilites"
-    nmap –script smb-check-vulns.nse –script-args=unsafe=1 -p445 $1
+    nmap --script smb-check-vulns.nse --script-args=unsafe=1 -p445 $1
     
     # Run enum4linux to get everything else including rid cycling etc
     echo "[*] Running enum4Linux to see what else we can find"
@@ -31,6 +31,4 @@ if [ -z "$1" ]
     echo "[*] and as a final step, simply setting up a brute force"
     nmap --script smb-brute.nse -p445 $1
 fi
-
-
 
