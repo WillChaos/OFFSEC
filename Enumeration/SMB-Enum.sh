5ba0fbc660151a2ce9fc34f354e8863549023e34
WillChaos <46702070+WillChaos@users.noreply.github.com>
2019-02-21 19:52:20 +0800

    Update SMB-Enum.sh

--- Enumeration/SMB-Enum.sh
+++ Enumeration/SMB-Enum.sh
@@ -17,6 +17,10 @@ if [ -z "$1" ]
     echo "[*] Looking up potential user names..."
     nmap --script smb-enum-users.nse --script-args=unsafe=1 -p445 $1
     
+    # checks for MS08-067
+    echo "[*] Checking exlusively for ms08-067"
+    nmap --script smb-vuln-ms08-067.nse -p445
+    
     # checks for MS17-010
     echo "[*] Checking exlusively for MS17-010"
     nmap -p 445 --script=smb-vuln-ms17-010.nse $1
@@ -28,6 +32,10 @@ if [ -z "$1" ]
     # Run enum4linux to get everything else including rid cycling etc
     echo "[*] Running enum4Linux to see what else we can find"
     enum4linux -v -a $1
+    
+    # check if Double pulsar backdoor is present
+    echo "[*] Checking to see if target has a double pulsar backdoor present..."
+    nmap -p 445 $1 --script=smb-double-pulsar-backdoor 
     
     echo "[*] and as a final step, simply setting up a brute force"
     nmap --script smb-brute.nse -p445 $1
