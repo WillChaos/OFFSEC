18ea1dda6f3bd040d8d09a05ff6934f31b569189
WillChaos <46702070+WillChaos@users.noreply.github.com>
2019-03-14 17:40:23 +0800

    Create procmon.sh
    monitor new spawned proccesses with a filter

---
+++ Tools/procmon.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#############################################################
+#  Script built to identify newly spawned proccess + filters
+############################################################
+
+if [ -z "$1" ]
+  then
+    echo "dont forget to pass in a filter..."
+    echo "usage: ./procmon.sh jason.sh" 
+  else
+
+# set the IFS to delimit results with correct spacing
+IFS=$'\n'
+
+# get current proccesses
+baseline_proc=$(ps -eo command)
+
+# begin identifying any new spawned procs
+while true; do
+    current_proc=$(ps -eo command)
+    diff <(echo "$baseline_proc") <(echo "$current_proc")| grep [\<\>] | grep $1
+    sleep 1
+    baseline_proc=$current_proc
+done
+
+fi
