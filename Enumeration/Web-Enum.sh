a3c23003644fca7d4d263beeb03de2d46966eeac
WillChaos <46702070+WillChaos@users.noreply.github.com>
2019-05-31 07:49:10 -0400

    Update Web-Enum.sh

--- Enumeration/Web-Enum.sh
+++ Enumeration/Web-Enum.sh
@@ -52,17 +52,14 @@ if [ $# -ne 2 ]
         gobuster -e -u $1:$2 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -o $1-$2-GOBUSTER.txt &
         
         echo "[*] Checking if wordpress exists..."
-        WP=$(validate_url $1:$2/wp-login.php >/dev/null)
-        if [ $WP ] 
-        then
+        
+        if validate_url $1:$2/wp-login.php; then
             echo "[!] Found wordpress indicated by login page here: $1:$2/wp-login.php"
             echo "[!] Running backgrounded wpscan"
             wpscan --url $1:$2--enumerate vp, vt, cb ,u -t 10 --output wordpress-$1:$2-wp.txt  --force --update --random-user-agent &
             
         else
-            WP2=$(validate_url $1:$2/wp-login.php >/dev/null)
-            if [ $WP2 ]
-            then
+            if validate_url $1:$2/wp/wp-login.php; then
                 echo "[!] Found wordpress indicated by login page here: $1:$2/wp/wp-login.php"
                 echo "[!] Running backgrounded wpscan"
                 wpscan --url $1:$2/wp --enumerate vp, vt, cb ,u -t 10 --output wordpress-$1:$2-wp.txt --force --update --random-user-agent &
