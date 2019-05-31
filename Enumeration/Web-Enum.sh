b8b2412a06d5a104b153204fc8c97aadf55fbf37
WillChaos <46702070+WillChaos@users.noreply.github.com>
2019-05-31 06:28:22 -0400

    Update Web-Enum.sh

--- Enumeration/Web-Enum.sh
+++ Enumeration/Web-Enum.sh
@@ -1,6 +1,24 @@
 #!/bin/bash 
 
+##############################################################################################################
+#
+# Description: Some hella simple web checks i always run. 
+#              nothing fancy, and a bit of a dodgie homejob script!
+# Author:      WillChaos
+# Usage:       Example: ./Web-enum.sh x.x.x.x 443
+#############################################################################################################
 
+
+
+# ------------------------------------------------ Functions -------------------------------------------------
+
+# if `validate_url $url >/dev/null`; then dosomething; else echo "does not exist"; fi
+# cred: https://gist.github.com/hrwgc/7455343
+function validate_url(){
+  if [[ `wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then echo "true"; fi
+}
+
+# -------------------------------------------------- Exec ----------------------------------------------------
 if [ $# -ne 2 ] 
     then
         echo "Please select scan host: Web-enum.sh <targetIP>  <port>"
@@ -31,13 +49,29 @@ if [ $# -ne 2 ]
         nmap $1 -p $2 --scrip=http-auth-finder.nse -Pn > $1-$2-AUTHSPIDER.txt &
 
         echo "[*] Running gobuster and outputing to > $1-$2.txt - backgrounded job"
-        gobuster -e -u $1:$2 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt > $1-$2-GOBUSTER.txt &
+        gobuster -e -u $1:$2 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -o $1-$2-GOBUSTER.txt &
+        
+        echo "[*] Checking if wordpress exists..."
+        if [ validate_url $1:$2/wp-login.php >/dev/null] 
+        then
+            echo "[!] Found wordpress indicated by login page here: $1:$2/wp-login.php"
+            echo "[!] Running backgrounded wpscan"
+            wpscan --url $1:$2--enumerate vp, vt, cb ,u -t 10 --output wordpress-$1:$2-wp.txt  --force --update --random-user-agent &
+            
+        else
+            if [ validate_url $1:$2/wp/wp-login.php >/dev/null]
+                echo "[!] Found wordpress indicated by login page here: $1:$2/wp/wp-login.php"
+                echo "[!] Running backgrounded wpscan"
+                wpscan --url $1:$2/wp --enumerate vp, vt, cb ,u -t 10 --output wordpress-$1:$2-wp.txt --force --update --random-user-agent &
+            else
+                echo "[X] Wordpress not detected
+            fi
+        fi
 
         echo "[*] Running Nikto scan"
         nikto -host $1 -port $2
 
-        echo "[!]TODO: add CMS dection (and everything else really.. lehl)"
-        # such ambition, not many reuslts, wow
+        
 
 
  fi
